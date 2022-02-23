5405ccee39a55855464a270dfafb65a18c05f4af
Iuliia El Sammak <juliag875@gmail.com>
2022-02-23 17:00:12 -0500

    commit tree 99c5f4cae15ca901e8fdfd6b778ec1deb3403358

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,21 @@
+class Book 
+  attr_reader :title
+  attr_accessor :author, :page_count, :genre
 
+  def initialize(title)
+    @title = title
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+end
+
+
+# def author=(value)
+#   @author = value
+# end
+
+# b1 = Book.new("And Then There Were None")
+# puts b1.title
 
